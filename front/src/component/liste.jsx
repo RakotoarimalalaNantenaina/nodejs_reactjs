0c2a3dfdc0ee3598df60b55f9d0f715b8647f3c4
RakotoarimalalaNantenaina <naynah344@gmail.com>
2019-04-18 20:26:28 +1000

    premier modification

--- front/src/component/liste.jsx
+++ front/src/component/liste.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { confirmAlert } from 'react-confirm-alert';
 import 'react-confirm-alert/src/react-confirm-alert.css'; 
+import Ajouter from './ajouter'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
  class List extends Component {
@@ -19,7 +21,7 @@ componentDidMount() {
 
 // Récupère la liste des éléments de l'application Express
 getList = () => {
-  fetch('http://localhost:3000/list')
+  fetch('http://localhost:8080/list')
   .then(res => res.json())
   .then(list => this.setState({ list }))
 }
@@ -28,49 +30,86 @@ render() {
   const { list } = this.state;
 
   return (
-    <div className="App">
-
-      <button onClick={()=>{
-        document.getElementById("name").style.display = "block";
-      }}>Lister</button>
-
+    <div className="container">
+        <Ajouter/>
+      <br/>
+      <table className="table table-bordered">
+        <thead className="thead-dark">
+          <tr>
+          <th id="th1">Id</th>
+          <th id="th1">Nom</th>
+          <th id="th1">Prenom</th>
+          <th id="th1">Actions</th>
+          </tr>
+        </thead>
+        <tbody>
       
-      {/*Vérifiez si des objets sont trouvés */}
-      {list.length ? (
-        <ul >
-          {/* Rendre la liste des articles */}
-          {list.map((item) => {
-            return(
-              <div id="name">
-                <li id="listedonne">{item.nom} &nbsp; {item.Prenom} <button onClick={()=>{
+        {list.length ? (
+
+              list.map(item => (
+                <tr key={item.id}>
+                  <td id="td1"><center>{item.id}</center></td>
+                  <td id="td1"><center>{item.nom}</center></td>
+                  <td id="td1"><center>{item.Prenom}</center></td>
+                <td>
+                <button onClick={()=>{
                     confirmAlert({
                       customUI: ({ onClose }) => {
                         return (
-                          <form method="POST" action="http://localhost:3000/list?_method=PUT" enctype="application/x-www-form-urlencoded">
-                            <div className='custom-ui'>
+                          <div className='custom-ui' id="modifier">
+                           <form method="POST" action="http://localhost:8080/list?_method=PUT" enctype="application/x-www-form-urlencoded">
+                              <input type="hidden" name="_method" value="PUT"/>
+                              <table>
+                                <tr>
+                                  <td><label>Nom :</label><input id="inputmodifiernom" type="text" name="nom" placeholder={item.nom}/></td>
+                               </tr>
+                               <tr>
+                                  <td> <label>Prenom :</label><input id="inputmodifierprenom" type="text" name="Prenom" placeholder={item.Prenom}/></td>
+                               </tr>                          
                             
-                              <label>Nom : </label><input type="text" placeholder={item.nom} name="nom"/><br/><br/>
-                              <label>Prenom : </label><input type="text" placeholder={item.Prenom} name="Prenom"/><br/>
-                            
-                              <button  type="submit"> Modifier </button> &nbsp;
-                              <button onClick={onClose}>Annuler</button>
+                                  <input type="hidden" name="id" value={item.id}/>
+                                    <div id="bouttonmodifier">
+                                      <button className="btn btn-primary">OK</button>&nbsp;&nbsp;
+                                      <button onClick={onClose} className="btn btn-secondary">Annuler</button>
+                                  </div>
+                              </table> 
+                            </form>
+
                           </div>
-                          </form>
                         );
                       }
                     })
                 }
-                }>modifier</button></li><br/>
-              </div>
-            );
-          })}
-        </ul>
-      ) : (
-        <div>
-          <h2>Aucune enregistrement</h2>
-        </div>
-      )
-    }
+                } className="btn btn-success">Edit</button>&nbsp;&nbsp;
+                
+                <button onClick = {()=>{
+                    confirmAlert({
+                     customUI: ({ onClose }) => {
+                       return (
+                         <div className='custom-ui'>
+                           <form method="POST" action="http://localhost:8080/list?_method=DELETE" enctype="application/x-www-form-urlencoded">
+                             <input type="hidden" name="_method" value="DELETE"/>
+                             <input type="hidden" name="id" value={item.id}/>
+
+                             <span>Confirmer la suppression<br/>{item.nom}</span>
+                             <br/><br/>
+                           <button className="btn btn-danger">OUI</button>&nbsp;&nbsp;
+                           <button onClick={onClose} className="btn btn-secondary">NON</button>
+                           </form>
+                         </div>
+                       );
+                     }
+                   });
+                  }} className="btn btn-danger">X</button> 
+                </td>
+                </tr>
+              ))
+              ) : (
+              <tr>	
+              </tr>
+              )}        
+    </tbody>
+    </table>
     </div>
   );
 }
